58d234cdd40baaa501327f932fe5d481fbcd062a
snranjith500@gmail.com <snranjithkumar500@gmail.com>
2026-08-31 21:59:02 +0530

    DAY 16 Notes on Debugging

---
+++ DAY-16/first.js
@@ -0,0 +1,44 @@
+
+
+
+// DAY -16  JS Debugging
+
+/**
+Programming is 60% Debugging
+
+when we try to implement a feature to our customers, we expect it meets theirs requiremtns yhat our end user looking for
+but often it happens that developing the application, we might miss out a few things, 
+it could be due to skills issue, lack of understanding the requirements,
+or technology, or maybe environment issues yhat may be not in our control
+
+
+In Breakpoint blue arrow 
+you can put it on beginning of the code expression not all
+F9 -->
+when we try execute line by line and when it come across any function calling, 
+its gonna go inside that function and execute that line by line then
+ only it comes back to the next line below function calling line
+
+F10 -)> wont go inside fn if called , moves to next line as expected
+
+F8 if you put debugger in line 5 and line 12 it going to directly jump 5 to 12, Continue execution until the next breakpoint.
+
+
+DOM Break points
+
+
+
+in VS CODE debug
+red dot
+
+1)Accept mistakes can happen
+2)Bigger Picture
+3)Debug Other's code
+4)Discuss approaches
+5)Tools
+6) Dont give up
+
+
+
+
+ */
